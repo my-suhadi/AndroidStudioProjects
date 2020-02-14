57bdc0d43e948e0e1cab76e0022f1d27d36b0ac9
my.suhadi <my.suhadi@gmail.com>
2020-02-15 00:30:31 +0700

    Update Design Section

---
+++ DesignSection/app/src/main/java/id/go/dephub/itjen/designsection/PostList.kt
@@ -0,0 +1,12 @@
+package id.go.dephub.itjen.designsection
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PostList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post_list)
+    }
+}
