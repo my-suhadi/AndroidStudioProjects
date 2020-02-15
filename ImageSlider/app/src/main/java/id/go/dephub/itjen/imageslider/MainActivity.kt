e25c1e83132a37c502d9a1e447d390772671443d
suhadi <my.suhadi@gmail.com>
2020-02-15 16:38:06 +0700

    Update Image Slider

--- ImageSlider/app/src/main/java/id/go/dephub/itjen/imageslider/MainActivity.kt
+++ ImageSlider/app/src/main/java/id/go/dephub/itjen/imageslider/MainActivity.kt
@@ -6,37 +6,47 @@ import android.widget.Button
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    // simpan semua gambar kedalam variabel array
+    val images = arrayOf(
+        R.drawable.slide_1,
+        R.drawable.slide_2,
+        R.drawable.slide_3,
+        R.drawable.slide_4,
+        R.drawable.slide_5,
+        R.drawable.slide_6,
+        R.drawable.slide_7)
+
+    var index = 0
+    lateinit var imageSlider :ImageView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // simpan semua gambar kedalam variabel array
-        val images = arrayOf(
-            R.drawable.slide_1,
-            R.drawable.slide_2,
-            R.drawable.slide_3,
-            R.drawable.slide_4,
-            R.drawable.slide_5,
-            R.drawable.slide_6,
-            R.drawable.slide_7)
-        var index = 0
-
         // fetch all of the views
-        val imageSlider = findViewById<ImageView>(R.id.image_slider)
+        imageSlider = findViewById(R.id.image_slider)
         val nextBtn = findViewById<Button>(R.id.next_btn)
         val prevBtn = findViewById<Button>(R.id.prev_btn)
 
         nextBtn.setOnClickListener {
-            index++
-            if (index == images.size) index = 0
-            imageSlider.setImageResource(images[index])
+            nextImage()
         }
 
         prevBtn.setOnClickListener {
-            if (index == 0) index = images.size
-            index--
-            imageSlider.setImageResource(images[index])
+            prevImage()
         }
+    }
+
+    fun nextImage() {
+        index++
+        if (index == images.size) index = 0
+        imageSlider.setImageResource(images[index])
+    }
+
+    fun prevImage() {
+        if (index == 0) index = images.size
+        index--
+        imageSlider.setImageResource(images[index])
     }
 }
