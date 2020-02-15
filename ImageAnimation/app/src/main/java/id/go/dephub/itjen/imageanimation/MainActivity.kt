7f57c0ec59abc2eb6b3cf33b02ceab00d50e11cb
suhadi <my.suhadi@gmail.com>
2020-02-15 19:28:59 +0700

    Update Image Animation

--- ImageAnimation/app/src/main/java/id/go/dephub/itjen/imageanimation/MainActivity.kt
+++ ImageAnimation/app/src/main/java/id/go/dephub/itjen/imageanimation/MainActivity.kt
@@ -1,12 +1,21 @@
 package id.go.dephub.itjen.imageanimation
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val animationImageView = findViewById<ImageView>(R.id.animation_imgView)
+        val backgroundAnimation = animationImageView.background as AnimationDrawable
+
+        backgroundAnimation.setEnterFadeDuration(3000)
+        backgroundAnimation.setExitFadeDuration(1000)
+        backgroundAnimation.start()
     }
 }
