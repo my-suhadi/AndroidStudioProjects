2d2f4298c1947a89d36b8416f6c8a83e533e637f
my.suhadi <my.suhadi@gmail.com>
2020-02-16 03:54:10 +0700

    Update Audios Android

--- AudiosAndroid/app/src/main/java/id/go/dephub/itjen/audiosandroid/MainActivity.kt
+++ AudiosAndroid/app/src/main/java/id/go/dephub/itjen/audiosandroid/MainActivity.kt
@@ -1,12 +1,32 @@
 package id.go.dephub.itjen.audiosandroid
 
+import android.graphics.Color
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun playMusic(v: View) {
+        val button = v as Button
+        val originalBackground = button.background
+        val mPlayer = MediaPlayer.create(this, resources.getIdentifier(button.tag as String, "raw", packageName))
+
+        mPlayer.start()
+
+        if (mPlayer.isPlaying) button.setBackgroundColor(Color.GREEN)
+
+        btn_stop.setOnClickListener {
+            mPlayer.reset()
+            button.background = originalBackground
+        }
     }
 }
