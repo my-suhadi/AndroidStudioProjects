e94cb24fb212329b14d04d1e970a71813c29ca93
my.suhadi <my.suhadi@gmail.com>
2020-02-16 23:54:58 +0700

    Update Videos Android

--- VideosAndroid/app/src/main/java/id/go/dephub/itjen/videosandroid/MainActivity.kt
+++ VideosAndroid/app/src/main/java/id/go/dephub/itjen/videosandroid/MainActivity.kt
@@ -2,11 +2,17 @@ package id.go.dephub.itjen.videosandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun playVideo(v : View) {
+        val buttonClicked = findViewById<Button>(R.id.btn_cancel)
     }
 }
