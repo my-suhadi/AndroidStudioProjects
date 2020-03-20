bccad7d739c74f6fcc5f0d2969a9bbdbf9126f40
my.suhadi <my.suhadi@gmail.com>
2020-03-21 02:09:17 +0700

    Update QuizApp

--- QuizApp/app/src/main/java/id/go/dephub/itjen/quizapp/QuizActivity.kt
+++ QuizApp/app/src/main/java/id/go/dephub/itjen/quizapp/QuizActivity.kt
@@ -1,18 +1,34 @@
 package id.go.dephub.itjen.quizapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.random.Random
 
 class QuizActivity : AppCompatActivity() {
+
+    val questions = arrayOf("web development", "game development", "web design", "networking", "video editing")
+    lateinit var rightAnswer: TextView
+    lateinit var question: TextView
+    lateinit var userAnswer: EditText
+    lateinit var randomNumber: Random
+    lateinit var currentQuestion: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        rightAnswer = findViewById(R.id.tView_rightAnswer)
+        question = findViewById(R.id.tView_question)
+        userAnswer = findViewById(R.id.edText_user_answer)
+
+        startQuiz()
     }
 
-    fun startQuiz() {
-
+    private fun startQuiz() {
+        currentQuestion = questions[Random.nextInt(questions.size)]
+        question.text = shuffleString(currentQuestion)
     }
 
     fun showAnswer(v: View) {
@@ -33,5 +49,16 @@ class QuizActivity : AppCompatActivity() {
 
     fun wrongAnswer() {
 
+    }
+
+    fun shuffleString(word: String): String {
+        val characters: CharArray = word.toCharArray()
+        for (i in characters.indices) {
+            val randomIndex = (Math.random() * characters.size).toInt()
+            val temp = characters[i]
+            characters[i] = characters[randomIndex]
+            characters[randomIndex] = temp
+        }
+        return String(characters)
     }
 }
