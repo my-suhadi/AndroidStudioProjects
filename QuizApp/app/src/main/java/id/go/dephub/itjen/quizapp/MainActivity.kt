5f789624ffa9349cc92bf23cfc5d0e9a92500b47
my.suhadi <my.suhadi@gmail.com>
2020-03-20 21:18:51 +0700

    Update QuizApp

--- QuizApp/app/src/main/java/id/go/dephub/itjen/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/id/go/dephub/itjen/quizapp/MainActivity.kt
@@ -1,12 +1,31 @@
 package id.go.dephub.itjen.quizapp
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.VideoView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.net.URI
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        playVideo()
+    }
+
+    fun openActivity(v: View) {
+        startActivity(Intent(this, QuizActivity::class.java))
+    }
+
+    fun playVideo() {
+        val videoPlayer = findViewById<VideoView>(R.id.video_background)
+        val videoUri = Uri.parse("android.resource://" + packageName + "/" + R.raw.video_one)
+        videoPlayer.setVideoURI(videoUri)
+        videoPlayer.start()
     }
 }
---
+++ QuizApp/app/src/main/java/id/go/dephub/itjen/quizapp/QuizActivity.kt
@@ -0,0 +1,37 @@
+package id.go.dephub.itjen.quizapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class QuizActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quiz)
+    }
+
+    fun startQuiz() {
+
+    }
+
+    fun showAnswer(v: View) {
+
+    }
+
+    fun checkAnswer(v: View) {
+
+    }
+
+    fun changeQuestion(v: View) {
+
+    }
+
+    fun rightAnswer() {
+
+    }
+
+    fun wrongAnswer() {
+
+    }
+}
