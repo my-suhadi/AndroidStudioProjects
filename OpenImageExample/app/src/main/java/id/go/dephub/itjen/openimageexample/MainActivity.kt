9bec823e65188d5d5ffb33535c5ee1fe769c4b2f
my.suhadi <my.suhadi@gmail.com>
2020-04-04 05:31:23 +0700

    initial commit opening image example

---
+++ OpenImageExample/app/src/main/java/id/go/dephub/itjen/openimageexample/MainActivity.kt
@@ -0,0 +1,34 @@
+package id.go.dephub.itjen.openimageexample
+
+import android.content.Intent
+import android.graphics.Bitmap
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val openButton = findViewById<Button>(R.id.btn_open)
+        openButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, 111)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == 111) {
+            val imageBitmap = data!!.extras!!.get("data") as Bitmap
+            val showImage = findViewById<ImageView>(R.id.iv_image)
+
+            showImage.setImageBitmap(imageBitmap)
+        }
+    }
+}
