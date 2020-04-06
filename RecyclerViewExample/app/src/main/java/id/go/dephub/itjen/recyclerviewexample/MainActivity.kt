ac606aa1d4d1ceca844ca0864696dedbf918bb00
my.suhadi <my.suhadi@gmail.com>
2020-04-06 08:48:52 +0700

    update RecyclerView Example

--- RecyclerViewExample/app/src/main/java/id/go/dephub/itjen/recyclerviewexample/MainActivity.kt
+++ RecyclerViewExample/app/src/main/java/id/go/dephub/itjen/recyclerviewexample/MainActivity.kt
@@ -2,6 +2,8 @@ package id.go.dephub.itjen.recyclerviewexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +16,26 @@ class MainActivity : AppCompatActivity() {
         // create an adapter (for normal class)
         // fill theRecycler in our activity
         // setOnClickedListener
+
+        val items = ArrayList<CustomItem>()
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+        items.add(CustomItem("Android App", R.drawable.image))
+
+        val myRecyclerView = findViewById<RecyclerView>(R.id.rv_recycler_data)
+        val myAdapter = MyRecyclerAdapter(items, applicationContext)
+
+        myRecyclerView.layoutManager = GridLayoutManager(applicationContext, 3)
+
+        myRecyclerView.adapter = myAdapter
     }
 }
--- RecyclerViewExample/app/src/main/java/id/go/dephub/itjen/recyclerviewexample/MyRecyclerAdapter.kt
+++ RecyclerViewExample/app/src/main/java/id/go/dephub/itjen/recyclerviewexample/MyRecyclerAdapter.kt
@@ -1,11 +1,15 @@
 package id.go.dephub.itjen.recyclerviewexample
 
+import android.app.Dialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 
 class MyRecyclerAdapter(data: ArrayList<CustomItem>, private val context: Context): RecyclerView.Adapter<MyRecyclerAdapter.CustomViewHolder>() {
@@ -22,10 +26,12 @@ class MyRecyclerAdapter(data: ArrayList<CustomItem>, private val context: Contex
 
         var image: ImageView
         var title: TextView
+        var card: CardView
 
         init {
             title = itemToView.findViewById(R.id.tv_item_title)
             image = itemToView.findViewById(R.id.ci_profile_image)
+            card = itemToView.findViewById(R.id.cv_card_profile)
         }
     }
 
@@ -43,5 +49,21 @@ class MyRecyclerAdapter(data: ArrayList<CustomItem>, private val context: Contex
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         holder.image.setImageResource(data[position].image)
         holder.title.text = data[position].title
+
+        holder.card.setOnClickListener {
+            val alertDialog = Dialog(context)
+            alertDialog.setContentView(R.layout.custom_alert)
+            alertDialog.window!!.setType(WindowManager.LayoutParams.TYPE_TOAST)
+            alertDialog.setTitle("Alert of Android Development")
+
+            alertDialog.findViewById<TextView>(R.id.tv_alert_label).text = data[position].title
+            alertDialog.findViewById<ImageView>(R.id.ci_alert_profile).setImageResource(data[position].image)
+
+            alertDialog.show()
+
+            alertDialog.findViewById<Button>(R.id.btn_alert_done).setOnClickListener {
+                alertDialog.dismiss()
+            }
+        }
     }
 }
