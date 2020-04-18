88d9894f8ad859d5aac4f79e52d934e08aee9120
suhadi <my.suhadi@gmail.com>
2020-04-18 08:54:27 +0700

    update Speech App

--- SpeechApp/app/src/main/java/id/go/dephub/itjen/speechapp/ItemAdapter.kt
+++ SpeechApp/app/src/main/java/id/go/dephub/itjen/speechapp/ItemAdapter.kt
@@ -2,6 +2,7 @@ package id.go.dephub.itjen.speechapp
 
 import android.app.Dialog
 import android.content.Context
+import android.os.Build
 import android.speech.tts.TextToSpeech
 import android.view.LayoutInflater
 import android.view.View
@@ -11,7 +12,10 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
+import kotlin.collections.ArrayList
 
 class ItemAdapter(_dataItems: ArrayList<ItemClass>, _cx: Context) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
@@ -26,7 +30,7 @@ class ItemAdapter(_dataItems: ArrayList<ItemClass>, _cx: Context) :
         internal val itemLayout = _itemOfView.findViewById<RelativeLayout>(R.id.item_layout)
         internal val itemImage = _itemOfView.findViewById<ImageView>(R.id.item_image)
         internal val itemName = _itemOfView.findViewById<TextView>(R.id.item_name)
-        internal lateinit var spell2Text: TextToSpeech
+        internal lateinit var text2Speech: TextToSpeech
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -38,9 +42,10 @@ class ItemAdapter(_dataItems: ArrayList<ItemClass>, _cx: Context) :
         return dataItems.size
     }
 
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        holder.itemImage.setImageResource(dataItems[position].imageResource)
-        holder.itemName.text = dataItems[position].name
+        holder.itemImage.setImageResource(this.dataItems[position].imageResource)
+        holder.itemName.text = this.dataItems[position].name
         holder.itemLayout.setOnClickListener {
             val myAlert = Dialog(this.cx)
             myAlert.setContentView(R.layout.alert)
@@ -54,7 +59,12 @@ class ItemAdapter(_dataItems: ArrayList<ItemClass>, _cx: Context) :
 
             val alertSpellBtn = myAlert.findViewById<Button>(R.id.alert_spell_btn)
             alertSpellBtn.setOnClickListener {
-
+                holder.text2Speech = TextToSpeech(cx, TextToSpeech.OnInitListener { status ->
+                    if (status == TextToSpeech.SUCCESS) {
+                        holder.text2Speech.isLanguageAvailable(Locale.ENGLISH)
+                        holder.text2Speech.speak(this.dataItems[position].name, TextToSpeech.QUEUE_FLUSH, null, null)
+                    }
+                })
             }
 
             val alertDoneBtn = myAlert.findViewById<Button>(R.id.alert_done_btn)
