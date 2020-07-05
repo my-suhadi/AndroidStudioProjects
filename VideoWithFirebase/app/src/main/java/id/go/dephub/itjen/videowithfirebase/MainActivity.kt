4b1e0e4d0a3b64379dd1c15b9314ea55dae2a21c
suhadi <my.suhadi@gmail.com>
2020-07-05 14:19:56 +0700

    Update VideoWithFirebase

--- VideoWithFirebase/app/src/main/java/id/go/dephub/itjen/videowithfirebase/MainActivity.kt
+++ VideoWithFirebase/app/src/main/java/id/go/dephub/itjen/videowithfirebase/MainActivity.kt
@@ -1,11 +1,21 @@
 package id.go.dephub.itjen.videowithfirebase
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun daftarBtnClicked(v: View) {
+        startActivity(Intent(this, RegisterActivity::class.java))
+    }
+
+    fun masukBtnClicked(v: View) {
+        startActivity(Intent(this, LoginActivity::class.java))
     }
 }
---
+++ VideoWithFirebase/app/src/main/java/id/go/dephub/itjen/videowithfirebase/ResetActivity.kt
@@ -0,0 +1,11 @@
+package id.go.dephub.itjen.videowithfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset)
+    }
+}
