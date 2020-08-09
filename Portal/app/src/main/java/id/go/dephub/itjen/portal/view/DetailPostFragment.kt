9e309cda8f7047e11c939d1094a6a6251cd0c054
my.suhadi <my.suhadi@gmail.com>
2020-08-09 15:27:03 +0700

    update portal-0.4 - navigation fragment menggunakan jetpack framework

---
+++ Portal/app/src/main/java/id/go/dephub/itjen/portal/view/DetailPostFragment.kt
@@ -0,0 +1,34 @@
+package id.go.dephub.itjen.portal.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import id.go.dephub.itjen.portal.R
+import kotlinx.android.synthetic.main.fragment_detail_post.*
+
+class DetailPostFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_post, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnKembali.setOnClickListener {
+            val actionKembali = DetailPostFragmentDirections.actionDetailPostFragmentToPostListFragment()
+            Navigation.findNavController(it).navigate(actionKembali)
+        }
+    }
+}
---
+++ Portal/app/src/main/java/id/go/dephub/itjen/portal/view/PostListFragment.kt
@@ -0,0 +1,36 @@
+package id.go.dephub.itjen.portal.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.ActionOnlyNavDirections
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import id.go.dephub.itjen.portal.R
+import kotlinx.android.synthetic.main.fragment_post_list.*
+
+class PostListFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_post_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnClick.setOnClickListener {
+            val actionToDetailPost = PostListFragmentDirections.actionPostListFragmentToDetailPostFragment()
+            Navigation.findNavController(it).navigate(actionToDetailPost)
+        }
+    }
+}
