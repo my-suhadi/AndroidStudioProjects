71426975b1aee8737b16f68dc138ed7a657655cb
suhadi <my.suhadi@gmail.com>
2020-11-25 06:51:21 +0700

    skemaraja - form fix

--- Skemaraja/app/src/main/java/id/go/dephub/itjen/skemaraja/MainActivity.kt
+++ Skemaraja/app/src/main/java/id/go/dephub/itjen/skemaraja/MainActivity.kt
@@ -3,10 +3,7 @@ package id.go.dephub.itjen.skemaraja
 import android.app.Activity
 import android.os.Build
 import android.os.Bundle
-import android.webkit.WebResourceError
-import android.webkit.WebResourceRequest
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
@@ -18,17 +15,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         web_view.webViewClient = CustomWebViewClient(this)
+        web_view.webChromeClient = CustomWebChromeClient(this)
+        true.also { web_view.settings.javaScriptEnabled = it }
         web_view.settings.userAgentString = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 " +
                 "(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
-        true.also {
-            web_view.settings.javaScriptEnabled = it
-        }
 
         web_view.loadUrl("https://dp-wfh.dephub.go.id/")
     }
 }
 
-class CustomWebViewClient internal constructor(private val activity: Activity) : WebViewClient() {
+class CustomWebChromeClient(private val activity: Activity) : WebChromeClient() {
+    override fun onGeolocationPermissionsShowPrompt(origin: String?, callback: GeolocationPermissions.Callback?) {
+         callback?.invoke(origin, true, true)
+    }
+
+}
+
+class CustomWebViewClient(private val activity: Activity) : WebViewClient() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
         val url: String = request?.url.toString();
@@ -46,15 +49,12 @@ class CustomWebViewClient internal constructor(private val activity: Activity) :
     }
 
     override fun onPageFinished(view: WebView, url: String?) {
-        true.also { view.settings.javaScriptEnabled = it }
-
-//        view.loadUrl("javascript:(function() {alert(\"Tes\");})()")
-
+        view.settings.setGeolocationEnabled(true)
         view.loadUrl(
             "javascript:(function() {" +
-                    "document.getElementsByName('nip').value = '198201192008121001';" +
-                    "document.getElementsByName('password').value = '198201192008121001';" +
-                    "document.getElementsByName('status_wfh').value = '3';" +
+                    "document.getElementsByName('nip')[0].value = '198201192008121001';" +
+                    "document.getElementsByName('password')[0].value = '198201192008121001';" +
+                    "document.getElementsByName('status_wfh')[0].value = '3';" +
                     "document.getElementById('timezone').value = 'Asia/Jakarta';" +
                     "document.getElementById('location_user').value = '';" +
                     "document.getElementById('location_status').value = '';" +
@@ -63,8 +63,5 @@ class CustomWebViewClient internal constructor(private val activity: Activity) :
         )
 
         Toast.makeText(activity, url, Toast.LENGTH_SHORT).show()
-
-//        web_view.loadUrl("javascript:(function() {document.getElementById('password').value = '198201192008121001';})()")
-//        web_view.loadUrl("javascript:(function() {let z = document.getElementById('btnSubmit').click();})()");
     }
 }
